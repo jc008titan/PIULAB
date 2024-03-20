2d735ed5b94d7f97b8824e557f0d7fa787a7e040
Andrei <Andrei@DESKTOP-E459JU9>
2024-03-20 20:47:57 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Farmacie
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Farmacie.Farmacie farmacie = new Farmacie.Farmacie();
+
+            string optiune;
+            do
+            {
+                Console.WriteLine("C. Citire medicamente de la tastatura");
+                Console.WriteLine("A. Afisarea medicamentelor");
+                Console.WriteLine("K. Cautare dupa numele medicamentului");
+                Console.WriteLine("X. Inchidere program");
+
+                Console.WriteLine("Alegeti o optiune");
+                optiune = Console.ReadLine();
+
+                switch (optiune.ToUpper())
+                {
+                    case "C":
+                        break;
+                    case "A":
+                        break;
+                    case "K":
+                        break;
+                    case "X":
+                        return;
+                    default:
+                        break;
+                }
+            } while (optiune.ToUpper() != "X");
+
+            Console.ReadKey();
+        }
+
+    }
+}
